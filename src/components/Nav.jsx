2bfe316fa16cedd717cb1b4e8fb8b980a77e0950
Ole M. Røsand <77734811+omrosand@users.noreply.github.com>
2023-04-04 15:02:55 +0200

    pages for dropdown + icons + nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,25 +1,66 @@
 import { Link } from "react-router-dom";
+import {
+  FaUser,
+  FaHome,
+  FaBook,
+  FaUserFriends,
+  FaQuestionCircle,
+  FaCog,
+} from "react-icons/fa";
+import { HiLogout } from "react-icons/hi";
 
 const navElements = [
   {
     title: "Hjem",
     path: "/",
+    icon: <FaHome />,
   },
   {
     title: "Min Profil",
     path: "/minprofil",
+    icon: <FaUser />,
   },
   {
     title: "Mine Bøker",
     path: "/mineboker",
+    icon: <FaBook />,
+    dropdown: [
+      {
+        title: "Registrer",
+        path: "/registrerbok",
+        icon: <FaBook />,
+      },
+      {
+        title: "Leste bøker",
+        path: "/lesteboker",
+        icon: <FaBook />,
+      },
+      {
+        title: "Favoritter",
+        path: "/favoritter",
+        icon: <FaBook />,
+      },
+    ],
   },
   {
     title: "Venner",
     path: "/venner",
+    icon: <FaUserFriends />,
+  },
+  {
+    title: "Ofte stilte spørsmål",
+    path: "/oftestiltesporsmal",
+    icon: <FaQuestionCircle />,
   },
   {
     title: "Innstillinger",
     path: "/innstillinger",
+    icon: <FaCog />,
+  },
+  {
+    title: "Logg ut",
+    path: "/loggut",
+    icon: <HiLogout />,
   },
 ];
 
@@ -28,7 +69,8 @@ const Nav = () => {
     <nav className="mainNav">
       <ul>
         {navElements.map((element) => (
-          <li key={element.title}>
+          <li className="navElement" key={element.title}>
+            {element.icon}
             <Link to={element.path}>{element.title}</Link>
           </li>
         ))}
