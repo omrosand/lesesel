18b5b90eef883f843e190d8b822d34ebd136823e
Monacean <70483631+Monacean@users.noreply.github.com>
2023-04-21 11:22:44 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,10 +28,8 @@ const Home = () => {
           <article>
             <h3>Sjekk scoreboard</h3>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-              luctus ipsum in pulvinar ultricies. Donec dignissim massa sem, non
-              placerat mauris elementum ac. Praesent pharetra fermentum enim
-              vitae tempus. Ut maximus nisl vitae rhoncus ultricies.
+              Hvor mye har vennene dine lest? Hvor ligger du på scoreboard? Er du på pallen?  <br/>
+              Dette kan du sjekke her. 
             </p>
             <button>
               <Link to="/scoreboard" className="homeLink">
@@ -45,10 +43,9 @@ const Home = () => {
           <article>
             <h3>Ønsker å lese</h3>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-              luctus ipsum in pulvinar ultricies. Donec dignissim massa sem, non
-              placerat mauris elementum ac. Praesent pharetra fermentum enim
-              vitae tempus. Ut maximus nisl vitae rhoncus ultricies.
+              Har du funnet en bok du ønsker å lese, men du kan ikke begynne på den akkurat nå? 
+              Hva med å lagre bøkene i en liste, slik at når du er klar, kan du bare gå inn på listen å se hvilke bøker du har lagret. <br/>
+              På den måten glemmer du ikke hvilke bøker du har vært interessert i. 
             </p>
             <button>
               <Link to="/scoreboard" className="homeLink">
