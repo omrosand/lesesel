4822ae9f4ea5214bb67c46cec2f2072b7bc48c20
nattis <77730450+nattis@users.noreply.github.com>
2023-04-24 19:17:35 +0200

    myprofile

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,13 +1,37 @@
 import { Helmet } from 'react-helmet';
+import { useState, useEffect } from 'react';
+import { client } from "../utils/sanityclient";
 
 
 const MyProfile = () => {
+  const [users, setUsers] = useState(null) 
+
+    async function fetchUsers() {
+        const data = await client.fetch(`*[_type == "users"]`)
+        return data
+    }
+
+    async function getUsers() {
+        const data = await fetchUsers()
+        setUsers(data)
+    }
+
+    useEffect(() => {
+        getUsers()
+    }, [])
+    console.log(users)
+
   return (
     <>
-    <Helmet>
-      <title>Min profil</title>
-    </Helmet>
+      <Helmet>
+        <title>Min profil</title>
+      </Helmet>
       <h1>Min Profil</h1>
+      <img alt="avatar" />
+      <h2>{users?.username}'s profil</h2>
+
+      <h3>Min poengscore</h3>
+      <h3>Mine trofeer</h3>
     </>
   );
 };
