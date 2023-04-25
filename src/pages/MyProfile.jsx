52005037d6386aefcc9dd62b3b217090a60a11b3
nattis <77730450+nattis@users.noreply.github.com>
2023-04-25 13:36:22 +0200

    min profil

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -5,32 +5,37 @@ import { client } from "../utils/sanityclient";
 const MyProfile = () => {
   const [users, setUsers] = useState(null);
 
-  async function fetchUsers() {
-    const data = await client.fetch(`*[_type == "users"]`);
-    return data;
-  }
-
-  async function getUsers() {
-    const data = await fetchUsers();
-    setUsers(data);
-  }
-
   useEffect(() => {
-    getUsers();
+    async function fetchUsers() {
+      const query = `*[_type == "users" && _id == $userId][0] {
+        username, 
+        avatar {
+          asset-> {
+            url
+          }
+        }
+      }`
+      const userId = {userId: '21ff6829-cd5e-4d19-9ea6-10118b81ee96'}
+      const result = await client.fetch(query, userId)
+
+      setUsers(result)
+    }
+    fetchUsers()
   }, []);
 
   return (
-    <>
+    <article className="myProfile">
       <Helmet>
         <title>Min profil</title>
       </Helmet>
-      <h1>Min Profil</h1>
-      <img src={users?.avatar?.asset?.url} />
+      <img className="avatar" src={users?.avatar?.asset?.url} alt="Avatar"/>
       <h2>{users?.username}'s profil</h2>
 
       <h3>Min poengscore</h3>
+      <p>293</p>
+
       <h3>Mine trofeer</h3>
-    </>
+    </article>
   );
 };
 export default MyProfile;
