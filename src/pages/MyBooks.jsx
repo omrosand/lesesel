5734bc0fa6b939cc4843e36842b8fd01e0ff9fea
Natalie <natalism@hiof.no>
2023-04-28 12:14:15 +0200

    webp

--- src/pages/MyBooks.jsx
+++ src/pages/MyBooks.jsx
@@ -12,17 +12,17 @@ const MyBooks = () => {
       <section>
         <ul className="cardList">
           <li className="card">
-            <img src="../src/img/Sel11.png" alt="sel med bok"/>
+            <img src="../src/img/Sel11.webp" alt="sel med bok"/>
             <button>
               <Link to="/registrerbok">Registrer bok</Link>
             </button>
           </li>
           <li className="card">
-            <img src="../src/img/Sel12.png" alt="sel som blunker"/>
+            <img src="../src/img/Sel12.webp" alt="sel som blunker"/>
             <button>Leste bøker</button>
           </li>
           <li className="card">
-            <img src="../src/img/Sel6.png" alt="sel med sjokkert ansikt"/>
+            <img src="../src/img/Sel6.webp" alt="sel med sjokkert ansikt"/>
             <button>Ønsker å lese</button>
           </li>
         </ul>
