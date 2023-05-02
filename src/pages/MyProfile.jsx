d26b0ff7d46e4c19f7f96af39fef071ba89a12c3
nattis <77730450+nattis@users.noreply.github.com>
2023-05-02 12:27:33 +0200

    myprofile

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,32 +1,35 @@
 import { Helmet } from "react-helmet";
-import { useState } from "react";
-import Users from "../components/Users";
 
-const MyProfile = () => {
-  const [users, setUsers] = useState(null);
-
-  function handleUsersLoad(data) {
-    setUsers(data);
-  }
-
+const MyProfile = ({user}) => {
+  const sumScore = () => {
+    if (user?.books?.length > 0) {
+      let score = 0;
+      user.books.forEach((book) => {
+        if (book.pages) {
+          score += parseInt(book.pages);
+        }
+      });
+      return score;
+    }
+  };
+  
   return (
     <>
       <article className="myProfile">
         <Helmet>
           <title>Min profil</title>
         </Helmet>
-        <Users onUsersLoad={handleUsersLoad} />
-        {users && (
+        {user && (
           <>
             <img
               className="avatar"
-              src={users?.avatar?.asset?.url}
+              src={user?.avatar?.asset?.url}
               alt="Avatar"
             />
-            <h2>{users?.username}'s profil</h2>
+            <h2>{user?.username}'s profil</h2>
 
             <h3>Min poengscore</h3>
-            <p>293</p>
+            <p>{sumScore()}</p>
 
             <h3>Mine trofeer</h3>
           </>
