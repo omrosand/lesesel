c74a7e6927bdc6bdbfcabe9fe4a6bb3adbb40bc7
Ole M. Røsand <77734811+omrosand@users.noreply.github.com>
2023-05-16 15:51:45 +0200

    friends

--- src/pages/Friends.jsx
+++ src/pages/Friends.jsx
@@ -3,28 +3,36 @@ import { Helmet } from "react-helmet";
 import { client } from "../utils/sanityclient";
 
 const Friends = ({ user }) => {
-  const [friends, setFriends] = useState([]);
-
-  const testId = "4af45169-af7e-4fd4-abe5-1018e191d6aa";
+  const [friendshipStatus, setFriendshipStatus] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchFriendships = async () => {
       try {
-        const query = `*[_type == "friendship" && (inviter._ref == "${user._id}" || invitee._ref == "${user._id}")]{
+        const query = `*[_type == "friendship" && (inviter._ref == $userId || invitee._ref == $userId)]{
           _id,
-          inviter._ref,
-          invitee._ref,
-          accepted,
+          inviter->{_id},
+          invitee->{_id},
+          accepted
         }`;
-        const result = await client.fetch(query, userId);
-        console.log(result);
-        setFriends(result);
+        const params = { userId: user._id };
+        const result = await client.fetch(query, params);
+        setFriendshipStatus(result);
       } catch (error) {
         console.log(error);
+      } finally {
+        setLoading(false);
       }
     };
-    fetchFriendships();
+
+    if (user) {
+      fetchFriendships();
+    }
   }, [user]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
@@ -34,16 +42,10 @@ const Friends = ({ user }) => {
 
       <article className="pageCard">
         <h1>Venner</h1>
-        <button
-          onClick={() => {
-            console.log(user);
-          }}
-        >
-          test
-        </button>
-        <ul>{JSON.stringify(friends)}</ul>
+        <ul>{JSON.stringify(friendshipStatus)}</ul>
       </article>
     </>
   );
 };
+
 export default Friends;
